fe06bff2fa61938a38648eb0cfaeff37ca828804
Hudson Luiz da Silva Costa <hudson_hip.hop12@hotmail.com>
2018-10-22 19:39:28 -0300

    Adicionar arquivos de projeto.

---
+++ teste1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(teste1.Startup))]
+namespace teste1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
