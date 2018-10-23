20f5f6e8e64965c3f5b7e84273c048b2fb6f1d00
Hudson Luiz da Silva Costa <hudson_hip.hop12@hotmail.com>
2018-10-22 21:07:52 -0300

    Alterações no front end

---
+++ teste1/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace teste1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Logar(string email, string password)
+        {
+
+            if(email == "hudsonluis.costa@gmail.com" && password == "starwark11")
+            {
+                return RedirectToAction("Index","Home");
+            }
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
