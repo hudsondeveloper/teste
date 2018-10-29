dbe93fb1cdaff834ca8d1e7a9305353aaa861795
Hudson Luiz da Silva Costa <hudson_hip.hop12@hotmail.com>
2018-10-29 09:41:04 -0300

    atualizações

--- teste1/Global.asax.cs
+++ teste1/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
 
 namespace teste1
 {
@@ -13,6 +14,7 @@ namespace teste1
         protected void Application_Start()
         {
             Application["ContadorAcessos"] = 0;
+ 
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
